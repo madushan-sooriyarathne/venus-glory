c417813d252feaa6852cb96db051b7f5cb5a4513
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-10-22 17:39:44 +0530

    added font import links

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,7 +41,18 @@ class AppDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en-US">
-        <Head>{/* Meta and Link tags */}</Head>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display:ital,wght@0,300;0,400;1,400&family=Nunito:ital,wght@0,300;0,600;0,700;1,400&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
         <body>
           <Main />
           <NextScript />
