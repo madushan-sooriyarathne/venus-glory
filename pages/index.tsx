e28ad6bd77b21d28cedc7f97c30c289bb3ac98c2
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-10-22 13:03:56 +0530

    creted the index.tsx file

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const HomePage: NextPage = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Venus Glory</h1>
+    </div>
+  );
+};
+
+export default HomePage;
