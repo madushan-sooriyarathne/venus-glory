acb4488e84c54197b7edda3d53d9c2b68924f592
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-10-22 13:02:51 +0530

    added export statement

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import { AppProps } from "next/app";
-import Head from "next/head"
+import Head from "next/head";
 
-
-const App: React.FC<AppProps> = ({Component, pageProps}: AppProps): JSX.Element => {
-    return <>
-    <Head>
+const App: React.FC<AppProps> = ({
+  Component,
+  pageProps,
+}: AppProps): JSX.Element => {
+  return (
+    <>
+      <Head>
         <title>Venus Glory | Natural cosmetics for gentle skincare</title>
-    </Head>
-    <Component {...pageProps} />
+      </Head>
+      <Component {...pageProps} />
     </>
-}
+  );
+};
+
+export default App;
