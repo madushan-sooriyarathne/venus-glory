1ae4fadf01b526ce4cd22d02f07fd47c2d6bbf84
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-10-26 01:27:06 +0530

    added Hero component to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
+import PrimaryHeading from "@components/headings/primary-heading";
+import Page from "@components/layout/common/page";
+import Hero from "@components/layout/homepage/hero";
 import { NextPage } from "next";
 
 const HomePage: NextPage = (): JSX.Element => {
   return (
-    <div>
-      <h1>Venus Glory</h1>
-    </div>
+    <Page>
+      <Hero />
+    </Page>
   );
 };
 
