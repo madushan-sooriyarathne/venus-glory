01722b67cfad2dca742a5d7278937f4637a98f5f
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-10-26 01:26:43 +0530

    added Image type

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,11 @@
 declare global {
   type Alignment = "left" | "right" | "center" | "justify";
+
+  interface Image {
+    src: string;
+    blurUrl: string;
+    alt?: string;
+  }
 }
 
 export {};
