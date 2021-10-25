e93a1bdbfc0966a86e0dc2ac1229822b8db36c02
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-10-25 15:20:09 +0530

    changed the color and size

--- components/headings/primary-heading/styles.ts
+++ components/headings/primary-heading/styles.ts
@@ -6,12 +6,12 @@ interface HeadingProps {
 }
 
 const Heading = styled.h1<HeadingProps>`
-  font-size: 10rem;
+  font-size: 8rem;
   font-family: ${(props) => props.theme.fonts.secondary};
   font-weight: 400;
   letter-spacing: 1px;
   color: ${(props) =>
-    props.light ? props.theme.colors.primary : props.theme.colors.black};
+    props.light ? props.theme.colors.primary : props.theme.colors.blackLight};
 `;
 
 export { Heading };
