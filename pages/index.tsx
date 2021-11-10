6877e45df04d1cf3dd6e3111b41301af5083c2d7
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-11-11 01:49:38 +0530

    added ProductGrid to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import ImageContentSection from "@components/layout/common/image-content-section";
 import Page from "@components/layout/common/page";
+import ProductGrid from "@components/layout/common/product-grid";
 import BestSellers from "@components/layout/homepage/bestsellers-section";
 import Categories from "@components/layout/homepage/categories";
 import GenderSection from "@components/layout/homepage/gender-section";
@@ -13,6 +14,7 @@ const HomePage: NextPage = (): JSX.Element => {
       <Categories />
       <BestSellers />
       <GenderSection />
+      <ProductGrid title="Latest Products" route="/shop" />
       <ImageContentSection
         image={{ src: "/assets/img/perfumes.jpg", blurUrl: "" }}
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
