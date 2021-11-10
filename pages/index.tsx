48f4432eef1b9f600ed5c3ee7a66052c52e2a095
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-11-10 19:39:52 +0530

    added GenderSection to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import ImageContentSection from "@components/layout/common/image-content-section
 import Page from "@components/layout/common/page";
 import BestSellers from "@components/layout/homepage/bestsellers-section";
 import Categories from "@components/layout/homepage/categories";
+import GenderSection from "@components/layout/homepage/gender-section";
 import Hero from "@components/layout/homepage/hero";
 import { NextPage } from "next";
 
@@ -11,19 +12,12 @@ const HomePage: NextPage = (): JSX.Element => {
       <Hero />
       <Categories />
       <BestSellers />
+      <GenderSection />
       <ImageContentSection
-        image={{ src: "/assets/img/men.jpg", blurUrl: "" }}
+        image={{ src: "/assets/img/perfumes.jpg", blurUrl: "" }}
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-        heading="For Men"
-        route="/shop/categories/men"
-      />
-
-      <ImageContentSection
-        image={{ src: "/assets/img/men.jpg", blurUrl: "" }}
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-        heading="For Kids"
-        route="/shop/categories/kids"
-        reversed
+        heading="Perfumes"
+        route="/shop/categories/perfumes"
       />
     </Page>
   );
