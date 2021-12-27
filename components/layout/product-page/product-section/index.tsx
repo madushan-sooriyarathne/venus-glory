dfa078cc8c4191f13861517014835a52ecaa0187
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-12-27 21:43:12 +0530

    finished the ProductSection component for now

--- components/layout/product-page/product-section/index.tsx
+++ components/layout/product-page/product-section/index.tsx
@@ -8,45 +8,152 @@ import {
   ProductContainer,
   ProductInfoSection,
   ProductInfoContainer,
+  ProductInfoCard,
   ProductInfoNavigation,
   ProductInfoNavButton,
   ProductActionSection,
+  ProductMetadata,
+  CategoryLabel,
+  ProductSizeLabel,
+  productInfoCardVariants,
+  ProductTag,
+  ItemGroup,
+  AddToFavoriteButton,
+  PriceTag,
+  PreviousPrice,
 } from "./styles";
+import ProductImageSet from "../product-image-set";
+import { useState } from "react";
+import ProductDescription from "../product-description";
+import ProductIngredients from "../product-ingredients";
+import ProductUsage from "../product-usage";
 
 interface Props {
   product: Product;
 }
 
+type ProductInfoNavType = "images" | "description" | "ingredients" | "howtouse";
+
+const navLinks: { id: string; label: string }[] = [
+  { id: "images", label: "Images" },
+  { id: "description", label: "Description" },
+  { id: "ingredients", label: "Ingredients" },
+  { id: "howtouse", label: "How to Use" },
+];
+
 const ProductSection: React.FC<Props> = ({ product }: Props): JSX.Element => {
+  const [selection, setSelection] = useState<ProductInfoNavType>("images");
+  const [fav, setFav] = useState<boolean>(false);
+
+  const toggleFav = () => {
+    setFav((prevState) => !prevState);
+  };
+
   return (
     <ProductContainer>
       <ProductInfoSection>
         <ProductInfoContainer>
-          <ImageComponent image={product.image}></ImageComponent>
-          <div>
-            <ReactMarkdown>{product.description}</ReactMarkdown>
-          </div>
+          {selection === "images" && (
+            <ProductInfoCard
+              variants={productInfoCardVariants}
+              initial="initial"
+              animate="enter"
+              exit="exit"
+            >
+              <ProductImageSet
+                images={[product.image, ...product.otherImages]}
+              />
+            </ProductInfoCard>
+          )}
+
+          {selection === "description" && (
+            <ProductInfoCard
+              variants={productInfoCardVariants}
+              initial="initial"
+              animate="enter"
+              exit="exit"
+            >
+              <ProductDescription description={product.description} />
+            </ProductInfoCard>
+          )}
+          {selection === "ingredients" && (
+            <ProductInfoCard
+              variants={productInfoCardVariants}
+              initial="initial"
+              animate="enter"
+              exit="exit"
+            >
+              <ProductIngredients
+                productName={product.name}
+                ingredients={product.ingredients}
+              />
+            </ProductInfoCard>
+          )}
+
+          {selection === "howtouse" && (
+            <ProductInfoCard
+              variants={productInfoCardVariants}
+              initial="initial"
+              animate="enter"
+              exit="exit"
+            >
+              <ProductUsage
+                productName={product.name}
+                usageInstructions={product.usage}
+              />
+            </ProductInfoCard>
+          )}
         </ProductInfoContainer>
         <ProductInfoNavigation>
-          <ProductInfoNavButton selected={false}>Images</ProductInfoNavButton>
-          <ProductInfoNavButton selected={true}>
-            Description
-          </ProductInfoNavButton>
-          <ProductInfoNavButton selected={true}>
-            Ingredients
-          </ProductInfoNavButton>
-          <ProductInfoNavButton selected={true}>
-            How to use
-          </ProductInfoNavButton>
+          {navLinks.map((link) => (
+            <ProductInfoNavButton
+              selected={selection === link.id}
+              onClick={() => setSelection(link.id as ProductInfoNavType)}
+            >
+              {link.label}
+            </ProductInfoNavButton>
+          ))}
         </ProductInfoNavigation>
       </ProductInfoSection>
       <ProductActionSection>
-        <PrimaryHeading small align="left">
-          {product.name}
-        </PrimaryHeading>
-        <ActionButton onClick={() => alert("Product added to the cart")}>
-          Add to cart
-        </ActionButton>
+        <ProductMetadata>
+          <ItemGroup gap={2}>
+            {product.bestseller && <ProductTag>Bestseller</ProductTag>}
+            {product.new && <ProductTag>New Arrival</ProductTag>}
+          </ItemGroup>
+          <PrimaryHeading small align="left">
+            {product.name}
+          </PrimaryHeading>
+          <ItemGroup gap={2}>
+            <CategoryLabel>{product.category.name}</CategoryLabel>
+            <ProductSizeLabel>{product.size}</ProductSizeLabel>
+          </ItemGroup>
+        </ProductMetadata>
+        <ItemGroup gap={1} align="flex-end">
+          {product.pricing.discount ? (
+            <>
+              <PriceTag>{`${product.pricing.currency} ${Math.round(
+                product.pricing.price -
+                  product.pricing.price * (product.pricing.discount / 100)
+              )}`}</PriceTag>
+              <PreviousPrice>{`${product.pricing.currency} ${product.pricing.price}`}</PreviousPrice>
+            </>
+          ) : (
+            <PriceTag>{`${product.pricing.currency} ${product.pricing.price}`}</PriceTag>
+          )}
+        </ItemGroup>
+        <ItemGroup gap={3}>
+          <ActionButton onClick={() => alert("Product added to the cart")}>
+            Add to cart
+          </ActionButton>
+          <AddToFavoriteButton onClick={toggleFav}>
+            <use
+              xlinkHref={`/assets/svg/sprites.svg#${
+                fav ? "fav-filled" : "fav"
+              }`}
+            />
+          </AddToFavoriteButton>
+        </ItemGroup>
       </ProductActionSection>
     </ProductContainer>
   );
--- components/layout/product-page/product-section/styles.ts
+++ components/layout/product-page/product-section/styles.ts
@@ -1,12 +1,11 @@
 import styled from "styled-components";
+import { motion, Variants } from "framer-motion";
 
 const ProductContainer = styled.section`
   grid-column: full-start / full-end;
   width: 100%;
   height: auto;
-  min-height: 100vh;
   overflow: hidden;
-  /* scrollbar-width: 0; */
 
   display: grid;
   grid-template-columns: minmax(min-content, 3fr) minmax(min-content, 2fr);
@@ -17,7 +16,7 @@ const ProductContainer = styled.section`
 const ProductInfoSection = styled.div`
   grid-area: im;
   width: 100%;
-  height: auto;
+  height: 100vh;
   padding: 10rem;
   overflow: hidden;
 
@@ -33,16 +32,41 @@ const ProductInfoContainer = styled.div`
   grid-column: 1 / 2;
 
   width: 100%;
-  height: 100%;
+  height: calc(100vh - 10rem);
 
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
+  overflow-y: auto;
+  overflow-x: hidden;
+  scrollbar-width: 1rem;
+  scrollbar-color: black;
+
+  &::-webkit-scrollbar {
+    width: 0.5rem;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: ${(props) => props.theme.colors.primary};
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: ${(props) => props.theme.colors.primaryDark};
+    width: 100%;
+  }
 
   & > * {
-    padding: 5rem 0;
+    padding: 0 1rem;
   }
+`;
+
+const ProductInfoCard = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
 `;
 
 const ProductInfoNavigation = styled.div`
@@ -66,9 +90,40 @@ const ProductInfoNavButton = styled.a<ProductInfoNavButtonProps>`
   outline: none;
   text-decoration: none;
   color: ${(props) =>
-    props.selected ? props.theme.colors.black : props.theme.colors.blackLight};
+    props.selected
+      ? props.theme.colors.blackLight
+      : props.theme.colors.primaryDark};
+
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    bottom: -2px;
+    left: 0;
+    right: 0;
+    background-color: ${(props) =>
+      props.selected
+        ? props.theme.colors.blackLight
+        : props.theme.colors.primaryDark};
+    transform: scaleX(0);
+    transition: transform 0.3s ease-in-out;
+    transform-origin: right center;
+  }
+
+  &:hover {
+    &::after {
+      transform: scaleX(1);
+      transform-origin: left center;
+    }
+  }
+
+  cursor: pointer;
 `;
 
+// Right hand side section
 const ProductActionSection = styled.div`
   grid-area: de;
   width: 100%;
@@ -80,16 +135,126 @@ const ProductActionSection = styled.div`
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
-  row-gap: 5rem;
+  row-gap: 10rem;
 
   background-color: ${(props) => props.theme.colors.primary};
 `;
+
+const ProductMetadata = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 1rem;
+`;
+
+const CategoryLabel = styled.h4`
+  font-size: 1.6rem;
+  font-weight: 700;
+  letter-spacing: 1px;
+  text-align: left;
+  text-transform: uppercase;
+  color: ${(props) => props.theme.colors.blackLight};
+`;
+
+const ProductSizeLabel = styled.p`
+  font-size: 1.3rem;
+  font-weight: 700;
+  letter-spacing: 0.5px;
+  text-align: left;
+  padding: 0.5rem 1rem;
+  color: ${(props) => props.theme.colors.white};
+  background-color: ${(props) => props.theme.colors.blackLight};
+  border-radius: 1px;
+`;
+
+const ProductTag = styled.p`
+  font-size: 1.4rem;
+  font-weight: 700;
+  letter-spacing: 0.5px;
+  text-align: left;
+  text-transform: uppercase;
+  padding: 0.5rem 1rem;
+  color: ${(props) => props.theme.colors.white};
+  background-color: ${(props) => props.theme.colors.primaryDark};
+  border-radius: 1px;
+`;
+
+const PriceTag = styled.p`
+  font-size: 4rem;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: 1px;
+  font-family: ${(props) => props.theme.fonts.secondary};
+  text-align: left;
+  color: ${(props) => props.theme.colors.blackLight};
+`;
+
+const PreviousPrice = styled.p`
+  font-size: 3rem;
+  font-weight: 400;
+  font-style: italic;
+  text-decoration: line-through;
+  letter-spacing: 1px;
+  font-family: ${(props) => props.theme.fonts.secondary};
+  text-align: left;
+  color: ${(props) => props.theme.colors.gray};
+`;
+
+interface ItemGroupProps {
+  gap: number;
+  align?: "flex-start" | "flex-end" | "center";
+}
+
+const ItemGroup = styled.div<ItemGroupProps>`
+  display: flex;
+  align-items: ${(props) => props.align || "center"};
+  justify-content: flex-start;
+  column-gap: ${(props) => `${props.gap}rem`};
+`;
+
+const AddToFavoriteButton = styled.svg`
+  width: 3rem;
+  height: 3rem;
+  fill: ${(props) => props.theme.colors.blackLight};
+  stroke: ${(props) => props.theme.colors.blackLight};
+  cursor: pointer;
+  transition: fill 0.2s ease-in-out, stroke 0.2s ease-in-out;
+
+  &:hover {
+    fill: ${(props) => props.theme.colors.primaryDark};
+    stroke: ${(props) => props.theme.colors.primaryDark};
+    transform: translate();
+  }
+`;
+
+const productInfoCardVariants: Variants = {
+  initial: {
+    opacity: 0,
+  },
+  enter: {
+    opacity: 1,
+  },
+  exit: {
+    opacity: 0,
+  },
+};
 
 export {
   ProductContainer,
   ProductInfoSection,
   ProductInfoContainer,
+  ProductInfoCard,
   ProductInfoNavigation,
   ProductInfoNavButton,
   ProductActionSection,
+  ProductMetadata,
+  CategoryLabel,
+  ProductSizeLabel,
+  ProductTag,
+  PriceTag,
+  PreviousPrice,
+  ItemGroup,
+  AddToFavoriteButton,
+  productInfoCardVariants,
 };
